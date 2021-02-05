28172f7fac3b2ca681cc237181c64f656ffc4d3c
Vaibhav Chiruguri <000vaibhav000@gmail.com>
2021-02-05 15:48:40 +0530

    Update response.go
    fix txhash parameter trimming 0

--- rpc/response.go
+++ rpc/response.go
@@ -117,7 +117,7 @@ func CheckParamString(r *http.Request, key string, defValue string) string {
 
 func CheckParamHex(r *http.Request, key string, defv []byte) []byte {
 	if v := r.FormValue(key); len(v) > 0 {
-		v = strings.TrimLeft(v, "0x")
+		//v = strings.TrimLeft(v, "0x")
 		hexdts, e := hex.DecodeString(v)
 		if e != nil {
 			return nil
