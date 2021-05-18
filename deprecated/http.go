5634fa20924b6ee7984e91fb19f0cd3ed6de7431
jojoin <yangjie@jojoin.com>
2021-05-18 17:42:46 +0800

    + desktop wallet: Create HAC ⇄ HACD swap transfer tx

--- deprecated/http.go
+++ deprecated/http.go
@@ -2,6 +2,7 @@ package rpc
 
 import (
 	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"os"
 	"strconv"
@@ -24,6 +25,25 @@ func (api *DeprecatedApiService) dealQuery(response http.ResponseWriter, request
 	// call controller
 	routeQueryRequest(params["action"], params, response, request)
 
+}
+
+func (api *DeprecatedApiService) dealOperateHex(response http.ResponseWriter, request *http.Request) {
+
+	bodyhexs, e1 := ioutil.ReadAll(request.Body)
+	if e1 != nil {
+		response.Write([]byte("body error"))
+		return
+	}
+	bodybytes, e0 := hex.DecodeString(string(bodyhexs))
+	if e0 != nil {
+		response.Write([]byte("body hex format error"))
+		return
+	}
+	if len(bodybytes) < 4 {
+		response.Write([]byte("body length less than 4"))
+		return
+	}
+	api.routeOperateRequest(response, binary.BigEndian.Uint32(bodybytes[0:4]), bodybytes[4:])
 }
 
 func (api *DeprecatedApiService) dealOperate(response http.ResponseWriter, request *http.Request) {
@@ -59,9 +79,10 @@ func (api *DeprecatedApiService) RunHttpRpcService(port int) {
 
 	mux.Handle("/websocket", websocket.Handler(api.webSocketHandler))
 
-	mux.HandleFunc("/", api.dealHome)           //设置访问的路由
-	mux.HandleFunc("/query", api.dealQuery)     //设置访问的路由
-	mux.HandleFunc("/operate", api.dealOperate) //设置访问的路由
+	mux.HandleFunc("/", api.dealHome)                 //设置访问的路由
+	mux.HandleFunc("/query", api.dealQuery)           //设置访问的路由
+	mux.HandleFunc("/operate", api.dealOperate)       //设置访问的路由
+	mux.HandleFunc("/operatehex", api.dealOperateHex) //设置访问的路由
 
 	//http.HandleFunc("/minerpool", minerPoolStatisticsAutoTransfer)       //设置访问的路由
 	//http.HandleFunc("/minerpool/transactions", minerPoolAllTransactions) //设置访问的路由
