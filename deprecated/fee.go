8bfb085798633913c8fc016e2b0e99781a2c93f2
jojoin <yangjie@jojoin.com>
2024-02-22 01:13:48 +0800

    up

--- deprecated/fee.go
+++ deprecated/fee.go
@@ -1,30 +1,85 @@
 package rpc
 
-import "strconv"
+import (
+	"fmt"
+	"github.com/hacash/core/interfaces"
+	"strconv"
+	"strings"
+)
+
+func omitMoreDecimalForFloatString(number string) string {
+	var pt = strings.IndexAny(number, ".")
+	if pt == -1 {
+		// not float with decimal
+		return number
+	}
+	var n = len(number)
+	var k = n
+	for i := pt + 1; i < n; i++ {
+		if number[i-1] != '0' && number[i] != '0' {
+			k = i + 1
+			break
+		}
+	}
+	return number[0:k]
+}
 
 func (api *DeprecatedApiService) getLatestAverageFeePurity(params map[string]string) map[string]string {
 	result := make(map[string]string)
 
-	txszp, ok1 := params["tx_size"]
+	_, isunitmei := params["unitmei"]
+	_, isomitdec := params["omitdec"]
+
+	txszp, ok1 := params["txsize"]
 	txsz := uint64(0)
 	if ok1 {
 		txsz, _ = strconv.ParseUint(txszp, 10, 64)
 	}
 
 	var kernel = api.blockchain.GetChainEngineKernel()
-	var feepb = uint64(kernel.GetLatestAverageFeePurity())
+	//var feepb = uint64(kernel.GetLatestAverageFeePurity())
+	//fmt.Println(feepb, "avgf feepb -------------")
+	var feert = getLatestAverageFeePurityData(kernel, isunitmei, uint32(txsz))
 
 	if txsz > 0 {
-		fp := feepb * (txsz/32 + 1)
-		fpn := strconv.FormatUint(fp, 10)
+		var fpn string
+		if isunitmei {
+			fpn = fmt.Sprintf("%f", feert) // mei
+		} else {
+			fpn = strconv.FormatUint(uint64(feert), 10) // zhu
+		}
+		//fpn = "17"+fpn+"72656897"
+		//fpn = "15.01"
+		if isomitdec {
+			fpn = omitMoreDecimalForFloatString(fpn)
+		}
 		result["feasible_fee"] = fpn
 
 	} else {
-		fpn := strconv.FormatUint(feepb, 10)
+		fpn := strconv.FormatUint(uint64(feert), 10)
 		result["fee_purity"] = fpn
 	}
 
 	// ok
 	return result
 
+}
+
+func getLatestAverageFeePurityData(kernel interfaces.ChainEngine, isunitmei bool, txsz uint32) float64 {
+
+	var feepb = uint64(kernel.GetLatestAverageFeePurity())
+	//fmt.Println(feepb, "avgf feepb -------------")
+
+	if txsz > 0 {
+		fp := feepb * (uint64(txsz)/32 + 1)
+		if isunitmei {
+			return float64(fp) / 10000_0000
+			//fpn = fmt.Sprintf("%f", fpf)// mei
+		} else {
+			//fpn = strconv.FormatUint(fp, 10) // zhu
+			return float64(fp)
+		}
+	} else {
+		return float64(feepb)
+	}
 }
--- deprecated/operate_tx.go
+++ deprecated/operate_tx.go
@@ -41,37 +41,38 @@ func (api *DeprecatedApiService) createTxAndCheckOrCommit(w http.ResponseWriter,
 	//fmt.Println(string(value))
 
 	// block chain
+	var e error
 	var kernel = api.backend.BlockChain().GetChainEngineKernel()
+	var tx interfaces.Transaction
+	var readability []string
 
-	tx, readability, e := createTransactionFromJson(kernel.StateRead(), value)
-	if e != nil {
-		w.Write([]byte(fmt.Sprintf(`{"error":"Create transaction error: %s"}`, e.Error())))
-		return
+	// get from txbody
+	txbdstr, e := jsonparser.GetString(value, "txbody")
+	if e == nil && len(txbdstr) > 4 {
+		txbts, e := hex.DecodeString(txbdstr)
+		if e == nil {
+			tx, _, _ = transactions.ParseTransaction(txbts, 0)
+		}
+	}
+
+	// parse from json
+	if tx == nil {
+		tx, readability, e = createTransactionFromJson(kernel, value)
+		if e != nil {
+			w.Write([]byte(fmt.Sprintf(`{"err":"Create transaction error: %s"}`, e.Error())))
+			return
+		}
 	}
 
 	ckstate, e := kernel.CurrentState().ForkSubChild()
 	if e != nil {
-		w.Write([]byte(fmt.Sprintf(`{"error":"Check Tx ForkSubChild error: %s"}`, e.Error())))
+		w.Write([]byte(fmt.Sprintf(`{"err":"Check Tx ForkSubChild error: %s"}`, e.Error())))
 		return
 	}
 	defer ckstate.Destory()
 
-	hashnofee := tx.Hash()
-	txbody, _ := tx.Serialize()
-	var txres = fmt.Sprintf(`,"txhash":"%s","txbody":"%s","description":["%s"]`,
-		hashnofee.ToHex(),
-		hex.EncodeToString(txbody),
-		strings.Join(readability, `","`),
-	)
-
-	// check tx with out signature
-	e = tx.WriteInChainState(ckstate)
-	if e != nil {
-		w.Write([]byte(fmt.Sprintf(`{"success":false%s,"error":"%s"}`, txres, e.Error())))
-		return
-	}
-
 	// commit tx to blockchain if have signature of main address
+	var isaddtopool = false
 	pubkey_str, e1 := jsonparser.GetString(value, "pubkey")
 	signature_str, e2 := jsonparser.GetString(value, "signature")
 	if e1 == nil && e2 == nil {
@@ -82,14 +83,43 @@ func (api *DeprecatedApiService) createTxAndCheckOrCommit(w http.ResponseWriter,
 				PublicKey: pubkey,
 				Signature: signature,
 			}})
+			/* txbody, _ := tx.Serialize()
+			fmt.Println( hex.EncodeToString(tx.Hash()),
+				hex.EncodeToString(txbody), len(txbody),
+				tx.GetSigns()[0].GetAddress().ToReadable(),
+				tx.GetSigns()[0].PublicKey.ToHex(),
+				tx.GetSigns()[0].Signature.ToHex(),
+				)*/
 			// Try to join the trading pool
 			e = api.txpool.AddTx(tx)
 			if e != nil {
 				e1 := fmt.Errorf("Add Tx to txpool error: %s", e.Error())
-				w.Write([]byte(fmt.Sprintf(`{"success":false%s,"error":"%s"}`, txres, e1.Error())))
+				//fmt.Println(e1.Error())
+				w.Write([]byte(fmt.Sprintf(`{"success":false,"err":"%s"}`, e1.Error())))
 				return
 			}
+			isaddtopool = true
 		}
+	}
+
+	hashnofee := tx.Hash()
+	txbody, _ := tx.Serialize()
+	var txres = fmt.Sprintf(`,"txhash":"%s","txhashfee":"%s","txbody":"%s","txfee":"%s","description":["%s"]`,
+		hashnofee.ToHex(),
+		tx.HashWithFee().ToHex(),
+		hex.EncodeToString(txbody),
+		tx.GetFee().ToFinString(),
+		strings.Join(readability, `","`),
+	)
+
+	if !isaddtopool {
+		// check tx with out signature
+		e = tx.WriteInChainState(ckstate)
+		if e != nil {
+			w.Write([]byte(fmt.Sprintf(`{"success":false%s,"err":"%s"}`, txres, e.Error())))
+			return
+		}
+
 	}
 
 	// ok
@@ -98,9 +128,10 @@ func (api *DeprecatedApiService) createTxAndCheckOrCommit(w http.ResponseWriter,
 }
 
 /**
- * createTransactionFromJson
+ * create Transaction From Json
  */
-func createTransactionFromJson(state interfaces.ChainStateOperationRead, jsonvalue []byte) (tx *transactions.Transaction_2_Simple, readability []string, err error) {
+func createTransactionFromJson(kernel interfaces.ChainEngine, jsonvalue []byte) (tx interfaces.Transaction, readability []string, err error) {
+	state := kernel.StateRead()
 	readability = make([]string, 0, 4)
 
 	var appendReadability = func(fmtstr string, a ...any) {
@@ -134,10 +165,13 @@ func createTransactionFromJson(state interfaces.ChainStateOperationRead, jsonval
 
 	// fee
 	fee, e := jsonGetAmt(jsonvalue, "fee")
-	if e != nil {
-		err = fmt.Errorf("Tx fee set error: %s", e.Error())
-		return
+	nopfee := false
+	if e != nil || fee == nil || !fee.IsPositive() {
+		// use recommon fee
+		nopfee = true
+		fee = fields.NewAmountByUnit(1, 244)
 	}
+	//fmt.Println("fee -------------- ", fee.ToFinString())
 	// addr
 	madr, e := jsonGetAddr(jsonvalue, "address")
 	if e != nil {
@@ -150,13 +184,14 @@ func createTransactionFromJson(state interfaces.ChainStateOperationRead, jsonval
 		tsnum = time.Now().Unix() // just now
 	}
 	// create tx
-	tx, e = transactions.NewEmptyTransaction_2_Simple(*madr)
+	txobj, e := transactions.NewEmptyTransaction_2_Simple(*madr)
 	if e != nil {
 		err = e
 		return
 	}
-	tx.Timestamp = fields.BlockTxTimestamp(uint64(tsnum))
-	tx.Fee = *fee
+	txobj.Timestamp = fields.BlockTxTimestamp(uint64(tsnum))
+	txobj.Fee = *fee
+	tx = txobj
 
 	// main address fee
 	appendReadability(`%s as executed account and pay %sHAC tx fee`, madr.ToReadable(), fee.ToMeiString())
@@ -188,7 +223,7 @@ func createTransactionFromJson(state interfaces.ChainStateOperationRead, jsonval
 			}
 			// tx append
 			act := actions.NewAction_1_SimpleToTransfer(*toadr, amt)
-			tx.AppendAction(act)
+			txobj.AppendAction(act)
 			// readability
 			appendReadability("Transfer %sHAC to %s", amt.ToMeiString(), toadr.ToReadable())
 			// ok
@@ -222,7 +257,7 @@ func createTransactionFromJson(state interfaces.ChainStateOperationRead, jsonval
 				return
 			}
 			//
-			tx.AppendAction(insact)
+			txobj.AppendAction(insact)
 			// readability
 			readtip := fmt.Sprintf("Write HACD inscription '%s' for %s",
 				insstr, diamonds.SerializeHACDlistToCommaSplitString())
@@ -239,6 +274,21 @@ func createTransactionFromJson(state interfaces.ChainStateOperationRead, jsonval
 	if parse_act_err != nil {
 		err = fmt.Errorf("Tx actions parse error: %s", parse_act_err.Error())
 		return
+	}
+
+	if nopfee {
+
+		addtxsize, _ := jsonparser.GetInt(jsonvalue, "addtxsize")
+		feezhu := getLatestAverageFeePurityData(kernel, false, tx.Size()+2+uint32(addtxsize))
+		fee = fields.NewAmountByUnit(int64(feezhu), 240)
+		fee, _, e = fee.CompressForMainNumLen(2, true)
+		if e != nil {
+			err = fmt.Errorf("Tx Fee CompressForMainNumLen error: %s", e.Error())
+			return
+		}
+		// reset fee
+		txobj.Fee = *fee
+		readability[0] = fmt.Sprintf(`%s as executed account and pay %sHAC tx fee`, madr.ToReadable(), fee.ToMeiString())
 	}
 
 	// ok
